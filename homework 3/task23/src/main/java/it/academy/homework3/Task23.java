5c6849ceaf060c9477248ce5178cbd02e37a5b8e
DMIT-00 <DMIT-00@localhost>
2022-11-18 10:39:55 +0300

    Main refactoring, added some methods for readability

--- homework 3/task23/src/main/java/it/academy/homework3/Task23.java
+++ homework 3/task23/src/main/java/it/academy/homework3/Task23.java
@@ -12,6 +12,16 @@ public class Task23 {
     public static void main(String[] args) {
         CarDaoImpl carDao = new CarDaoImpl(HibernateUtil.getSessionFactory());
 
+        UppercaseTrigger.createManufacturerUppercaseTrigger(HibernateUtil.getSessionFactory());
+        add5NewCars(carDao);
+        UppercaseTrigger.dropManufacturerUppercaseTrigger(HibernateUtil.getSessionFactory());
+
+        add1CarUsingDeleteAndCreate(carDao);
+
+        printEveryCarInDatabase(carDao);
+    }
+
+    public static void add5NewCars(CarDaoImpl carDao) {
         List<Car> cars = List.of(
                 new Car(null, "BMW", Date.valueOf("2020-02-08"), 1.0),
                 new Car(null, "KIA", Date.valueOf("2014-04-08"), 10.0),
@@ -20,19 +30,19 @@ public class Task23 {
                 new Car(null, "Ferrari", Date.valueOf("2022-04-04"), 42.0)
         );
 
-        UppercaseTrigger.createManufacturerUppercaseTrigger(HibernateUtil.getSessionFactory());
         cars.forEach(carDao::create);
-        UppercaseTrigger.dropManufacturerUppercaseTrigger(HibernateUtil.getSessionFactory());
+    }
 
-        carDao.readAll().forEach(System.out::println);
-
-        // Example of using flush() method
+    public static void add1CarUsingDeleteAndCreate(CarDaoImpl carDao) {
         Car car = new Car(null, "Mercedes", Date.valueOf("2020-02-08"), 10.0);
         carDao.create(car);
 
+        // Example of using flush() method
         // To create object again we need to delete it, then use flush() and create it again
         carDao.deleteAndCreate(car);
+    }
 
-        System.out.println(car);
+    public static void printEveryCarInDatabase(CarDaoImpl carDao) {
+        carDao.readAll().forEach(System.out::println);
     }
 }
